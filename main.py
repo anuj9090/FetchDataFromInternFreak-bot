2fd8a48a7b236820779f8b5b7aaef4db1a9aa883
Anuj Gupta <64634632+anuj9090@users.noreply.github.com>
2024-02-06 22:03:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -258,9 +258,9 @@ def greet(message):
   Content2 = "And much more! Only on internfreak.co \n \n Join the telegram channel for regular updates: https://bit.ly/3FElTyx \n \n #letssupportfreshers #offcampus #Jobs4u #offcampusdrive #Internships #Jobsforfreshers #softwareengineer #fullstackdeveloper #freshershiring #hiring #recruitment #opportunities #internfreak #faang #dell #offcampus"
   
   displaylinkedin = Content1 + Main +"\n"+ Content2 
-  bot.send_message(message.chat.id, f"`{text}`")
+  bot.send_message(message.chat.id, f"`{Main}`")
   time.sleep(3)
-  bot.send_message(message.chat.id, f"`{text}`")
+  bot.send_message(message.chat.id, f"`{displaylinkedin}`")
 
 @bot.message_handler(commands=['Send_To_Channel'])
 def Toss_A_Coin(msg):
